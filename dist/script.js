8305ebd71a0fc0c193bcddbfdf22745f1f955484
Ajeet <aeetsinghyadav2796@gmail.com>
2025-04-17 11:59:52 +0530

    Delete dist directory

--- dist/script.js
+++
@@ -1,6 +0,0 @@
-// You can add interactivity or animations here as per your requirements
-
-// Example: Add smooth scroll effect for the "Contact Us" button
-document.querySelector('.btn').addEventListener('click', function() {
-  window.location.href = 'mailto:truvitapharma@gmail.com';
-});
